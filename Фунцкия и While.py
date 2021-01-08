eeca4490b7118151d485865426113e986178bf0d
Makohonchuk Valentina <yaskiv79@gmail.com>
2021-01-08 20:47:29 +0200

    Фунцкия и While

--- Фунцкия и While.py
+++ Фунцкия и While.py
@@ -1,14 +1,15 @@
 def a_function():
     print("\nСравнение закончили")
+
+
 A = 1
 B = 4
-print("A=1, B=4")
+print("A = 1 B = 4")
 
 while A < B:
     print("А пока меньше В")
     A += 1
-    res = A + 1
-    print("\nA+1=", res)
+    print("\nA + 1 =", A)
     if A >= B:
-        print("\nА, наконец то, болше В!!!")
+        print("\nА, наконец-то, больше В!!!")
 a_function()
