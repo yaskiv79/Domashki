d5ab6adde9e94807616f56aadf1e1af814d06471
Makohonchuk Valentina <yaskiv79@gmail.com>
2021-01-12 17:54:18 +0200

    Фунцкия и While

--- Фунцкия и While.py
+++ Фунцкия и While.py
@@ -1,4 +1,5 @@
-def Comparing(K, B):
+def Comparing(K):
+    B = 8
     while K < B:
         print("K пока меньше В")
         K += 1
@@ -6,8 +7,8 @@ def Comparing(K, B):
         if K >= B:
             print("K, наконец-то, больше В!!!")
 
-
-Comparing(1, 10)
+for i in range (1, 10):
+    Comparing(i)
 
 
 
