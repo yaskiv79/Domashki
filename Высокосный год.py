30507054a5bd24cfdf0f8439bf95b2a1c56e855c
Makohonchuk Valentina <yaskiv79@gmail.com>
2021-01-24 23:00:13 +0200

    Высокосный год

---
+++ Высокосный год.py
@@ -0,0 +1,16 @@
+a = int(input())
+    high_year(a)
+def high_year():
+    b = a % 4
+    c = a % 100
+    d = a % 400
+    if b == 0:
+        if c == 0:
+            if d == 0:
+                 print('Год высокосный')
+            else:
+                print('Год не высокосный')
+        else:
+            print('Год высокосный')
+    else:
+        print('Год не высокосный')
